187358eb593d0b4f07ae8fa03da0a6e23b53713c
Sergey Pozniakevich <ven07@yandex.ru>
2022-08-09 01:28:24 +0700

    add square

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт его квадрат (число умноженное само на себя)
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+//int sqrt = number * number;
+// number *= number;
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("Число в квадрате: " + sqrt1);
